28db01d7440918a9c016b6b0356dc7553e9f4f36
Klavs Tranborg <kte@scanitech.dk>
2024-03-13 17:33:18 +0100

    Updated packages and added new database access classes
    Updated the `Tranborg.DataAccess` package from `1.0.3` to `1.0.4` and added new package references to the `DataAccess.csproj` file. These include `MySql.Data` version `8.3.0`, `Npgsql` version `8.0.2`, and `System.Drawing.Common` version `4.7.2`.
    
    Added a new class `MySQLDatabaseAccess` to the `MySQLDatabaseAccess.cs` file that implements the `IDataAccess` interface and provides methods for interacting with a MySQL database. Similarly, added a new class `PostgreSQLDatabaseAccess` to the `PostgreSQLDatabaseAccess.cs` file that also implements the `IDataAccess` interface and provides methods for interacting with a PostgreSQL database. Both classes include methods for loading data, loading single data, and saving data, each with synchronous and asynchronous versions.

---
+++ DataAccess/MySQLDatabaseAccess.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Class <c>MySQLDataAccess</c> is a class that implements the <c>IDataAccess</c> interface.
+    /// Used to interact with a MySQL database.
+    /// </summary>
+    public class MySQLDatabaseAccess : IDatabaseAccess
+    {
+        /// <summary>
+        /// Loads data from a MySQL database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public List<T> LoadData<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new MySqlConnection(connectionString);
+            var rows = connection.Query<T>(TSQL, parameters, commandType: commandType);
+            return rows.ToList();
+        }
+        /// <summary>
+        /// Loads data from a MySQL database asynchronously.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public async Task<List<T>> LoadDataAsync<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new MySqlConnection(connectionString);
+            var rows = await connection.QueryAsync<T>(TSQL, parameters, commandType: commandType);
+            return rows.ToList();
+
+        }
+        /// <summary>
+        /// Loads a single row of data from a MySQL database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public T LoadSingleData<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new MySqlConnection(connectionString);
+            var row = connection.QueryFirstOrDefault<T>(TSQL, parameters, commandType: commandType);
+            return row;
+        }
+
+        /// <summary>
+        /// Loads a single row of data from a MySQL database asynchronously.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public async Task<T> LoadSingleDataAsync<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new MySqlConnection(connectionString);
+            var row = await connection.QueryFirstOrDefaultAsync<T>(TSQL, parameters, commandType: commandType);
+            return row;
+        }
+
+        /// <summary>
+        /// Saves data to a MySQL database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        public void SaveData<T>(string TSQL, T parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new MySqlConnection(connectionString);
+            connection.Execute(TSQL, parameters, commandType: commandType);
+        }
+
+        /// <summary>
+        /// Saves data to a MySQL database asynchronously.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public async Task SaveDataAsync<T>(string TSQL, T parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new MySqlConnection(connectionString);
+            connection.ExecuteAsync(TSQL, parameters, commandType: commandType);
+        }
+    }
+}
---
+++ DataAccess/PostgreSQLDatabaseAccess.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
+namespace DataAccess
+{
+    /// <summary>
+    /// Class <c>PostgreSQLDataAccess</c> is a class that implements the <c>IDataAccess</c> interface.
+    /// Used to interact with a PostgreSQL database.
+    /// </summary>
+    public class PostgreSQLDatabaseAccess : IDatabaseAccess
+    {
+        /// <summary>
+        /// Loads data from a PostgreSQL database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public List<T> LoadData<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new NpgsqlConnection(connectionString);
+            var rows = connection.Query<T>(TSQL, parameters, commandType: commandType);
+            return rows.ToList();
+        }
+
+        /// <summary>
+        /// Loads data from a PostgreSQL database asynchronously.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public async Task<List<T>> LoadDataAsync<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new NpgsqlConnection(connectionString);
+            var rows = await connection.QueryAsync<T>(TSQL, parameters, commandType: commandType);
+            return rows.ToList();
+        }
+
+        /// <summary>
+        /// Loads a single row of data from a PostgreSQL database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public T LoadSingleData<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new NpgsqlConnection(connectionString);
+            var row = connection.QueryFirstOrDefault<T>(TSQL, parameters, commandType: commandType);
+            return row;
+        }
+
+        /// <summary>
+        /// Loads a single row of data from a PostgreSQL database asynchronously.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public async Task<T> LoadSingleDataAsync<T, U>(string TSQL, U parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new NpgsqlConnection(connectionString);
+            var row = await connection.QueryFirstOrDefaultAsync<T>(TSQL, parameters, commandType: commandType);
+            return row;
+        }
+
+        /// <summary>
+        /// Saves data to a PostgreSQL database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        public void SaveData<T>(string TSQL, T parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new NpgsqlConnection(connectionString);
+            connection.Execute(TSQL, parameters, commandType: commandType);
+        }
+
+        /// <summary>
+        /// Saves data to a PostgreSQL database asynchronously.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="TSQL"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="isStoredProcedure"></param>
+        /// <returns></returns>
+        public async Task SaveDataAsync<T>(string TSQL, T parameters, string connectionString, bool isStoredProcedure = false)
+        {
+            CommandType commandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+            using IDbConnection connection = new NpgsqlConnection(connectionString);
+            connection.ExecuteAsync(TSQL, parameters, commandType: commandType);
+        }
+    }
+}
