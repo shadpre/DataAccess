42302076fe17bd540c4f2c26638e2d874f5546aa
Klavs Tranborg <kte@scanitech.dk>
2024-03-13 16:24:27 +0100

    Updated package version and cleaned up using directives
    Updated the version number of the `Tranborg.DataAccess` package in the `DataAccess.csproj` file from `1.0.0.2` to `1.0.2`. In the `MSSQLDatabaseAccess.cs` file, removed several `using` directives including `Microsoft.Extensions.Configuration`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`. Also, moved the `using Dapper;` directive to the top of the file.

--- DataAccess/MSSQLDatabaseAccess.cs
+++ DataAccess/MSSQLDatabaseAccess.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
+﻿using Dapper;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using Dapper;
-using System.Threading.Tasks;
 
 namespace DataAccess
 {
